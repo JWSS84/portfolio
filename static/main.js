9eacd9024f0d817429bf935f880dfbd984474528
Jarbas w. Somonet <jarbaswssilva@gmail.com>
2022-08-05 15:39:44 -0300

    criado seção habilidades e ancoragem.

--- static/main.js
+++ static/main.js
@@ -9,3 +9,19 @@ menuMobile.addEventListener('click', () => {
     : menuMobile.classList.replace('bi-x', 'bi-list')
   body.classList.toggle('menu-nav-active')
 })
+
+/**
+ * fecha o menu quando clicar e muda o icone
+   quando algum item for clica será retirado a class menu active,
+   e trocado o icone do menu "X" ou "hamburguer"
+ */
+const navItem = document.querySelectorAll('.navItem')
+
+navItem.forEach(item => {
+  item.addEventListener('click', () => {
+    if (body.classList.contains('menu-nav-active')) {
+      body.classList.remove('menu-nav-active')
+      menuMobile.classList.replace('bi-x', 'bi-list')
+    }
+  })
+})
